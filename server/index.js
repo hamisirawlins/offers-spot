e14cddfba86674da557f4ab7775c72fcb06e3999
Hamisi Rawlins <53607315+hamisirawlins@users.noreply.github.com>
2021-11-15 16:52:31 +0300

    Dotenv notation

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import postRoutes from './routes/posts.js';
-
+import dotenv from 'dotenv';
 
 const app = express();
-
+dotenv.config();
 
 app.use(cors());
 app.use(bodyParser.json({limit:"30mb",extended:true}));
@@ -17,7 +17,7 @@ app.use('/posts',postRoutes);
 const CONNECTION_URL = 'mongodb+srv://javascript125:javascript125@cluster0.tjnul.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5001;
 
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
     .then(()=>app.listen(PORT, ()=>console.log(`Server is up on Port: ${PORT}`)))
     .catch((error)=>console.log(error.message));
  
